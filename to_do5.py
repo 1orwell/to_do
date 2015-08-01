6dffd4d8f84bd11c2be18d7636081d79dfb201ef
Elise Ratcliffe <elise@eliseratcliffe.co.uk>
2015-08-01 15:59:31 +0100

    final

--- to_do5.py
+++ to_do5.py
@@ -1,16 +1,28 @@
 import sys
-to_do_lst = []
+to_do_dct = {}
+number = 0
 
 if 'add' in sys.argv:
-    to_do_lst.append(' '.join(sys.argv[2:]))
-    with open('to_do', "r+") as f:
+    number += 1
+    to_do_dct[str(number)] = ' '.join(sys.argv[2:])
+    with open('to_do', "w+") as f:
+        for k, v in to_do_dct.items():
+            l = "{0}: {1}\n".format(k, v)
+            f.write(l)
+
+elif 'delete' or 'remove' in sys.argv:
+    num = sys.argv[2:]
+    for i in to_do_lst:
+        if i[0] == num:
+            to_do_lst.remove(i)
+    with open('to_do', "w+") as f:
         doc = f.read()
         to_do_str = str(to_do_lst)[2:-2]
         f.write(to_do_str)
         f.write('\n')
 
 elif 'print' in sys.argv:
-    with open('to_do', "r+") as f:
+    with open('to_do', "w+") as f:
         doc = f.read()
         print doc
 
